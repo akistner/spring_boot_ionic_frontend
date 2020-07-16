7049b44155650f2f59e8c5efddb2bdeb69782735
Anderson Kistner <andersonkistner@hotmail.com>
2020-07-16 13:10:24 -0300

    Dependências e package-lock.json reinstaladas

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: "https://ecommerce-spring-boot-ionic.herokuapp.com/",
+  baseUrl: "https://ecommerce-spring-boot-ionic.herokuapp.com",
   bucketBaseUrl: "https://ecommerce-spring-boot-ionic.s3-sa-east-1.amazonaws.com",
 }
